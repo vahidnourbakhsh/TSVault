f7553af549f731e8abb67ae5cb88c98fa062e021
Vahid Nourbakhsh <Vahid.Nourbakhsh@albertsons.com>
2023-04-26 12:50:49 -0700

    add docstring to series_to_supervised

--- utilities.py
+++ utilities.py
@@ -4,9 +4,20 @@ import pandas as pd
 from typing import List
 
 def series_to_supervised(data: pd.DataFrame, groupby_cols: List[str], value_cols: List[str],
-                         window=1, lag=1, dropnan=True):
+                         window=1, lag=1, dropnan=True) -> pd.DataFrame():
     """Use history according to `window` size to predict the future specified in `lag`.
     The output is a wide-format dataframe. This method creates (1, ..., window) features for each of the columns in `data`.
+
+    Args:
+        data (pd.DataFrame): Input dataframe
+        groupby_cols (List[str]): columns that are grouped. For example, if you are predicting demand for each (store, item), both store and item would be grouped_cols.
+        value_cols (List[str]): Columns used for prediction, meaning we look at the history of these columns according to `window` size.
+        window (int, optional): History window we would want to use for prediction (applies to `value_cols`). Defaults to 1.
+        lag (int, optional): Future period we want to predict (applies to `value_cols`). Defaults to 1.
+        dropnan (bool, optional): When we shift `val_cols` some values would be Null, by default the functions removes these records. Defaults to True.
+    
+    Returns:
+        pd.DataFrame(): a wide-format dataframe which includes all `val_cols` that are shifted according to `window`.
     """
     cols = list()
     
