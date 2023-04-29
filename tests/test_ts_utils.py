52e7227457c80b684b770ff36a633b0d58142d2d
Vahid Nourbakhsh <Vahid.Nourbakhsh@albertsons.com>
2023-04-29 09:09:53 -0700

    format with black

--- tests/test_ts_utils.py
+++ tests/test_ts_utils.py
@@ -7,20 +7,48 @@ from utilities.utilities import series_to_supervised
 
 
 def test_series_to_supervised():
-    df = pd.DataFrame({"store": [1, 1, 1, 2, 2, 2],
-    "date": [date(2022, 1, 1), date(2022, 1, 2), date(2022, 1, 3), date(2022, 1, 10), date(2022, 1, 11), date(2022, 1, 12)],
-    "sales": [100, 150, 200, 110, 165, 220]})
-    df2 = series_to_supervised(df, groupby_cols=["store"], value_cols=["sales"], window=1, lag=1, dropnan=True)
+    df = pd.DataFrame(
+        {
+            "store": [1, 1, 1, 2, 2, 2],
+            "date": [
+                date(2022, 1, 1),
+                date(2022, 1, 2),
+                date(2022, 1, 3),
+                date(2022, 1, 10),
+                date(2022, 1, 11),
+                date(2022, 1, 12),
+            ],
+            "sales": [100, 150, 200, 110, 165, 220],
+        }
+    )
+    df2 = series_to_supervised(
+        df, groupby_cols=["store"], value_cols=["sales"], window=1, lag=1, dropnan=True
+    )
     assert len(df2) == 2
     assert all(df2.iloc[0].values == [1, date(2022, 1, 2), 150, 100.0, 200.0])
     assert all(df2.iloc[1].values == [2, date(2022, 1, 11), 165, 110.0, 220.0])
 
 
 def test_series_to_supervised_2():
-    df = pd.DataFrame({"store": [1, 1, 1, 1, 2, 2, 2, 2],
-    "date": [date(2022, 1, 1), date(2022, 1, 2), date(2022, 1, 3), date(2022, 1, 4), date(2022, 1, 10), date(2022, 1, 11), date(2022, 1, 12), date(2022, 1, 13)],
-    "sales": [100, 150, 200, 250, 110, 165, 220, 260]})
-    df2 = series_to_supervised(df, groupby_cols=["store"], value_cols=["sales"], window=1, lag=1, dropnan=True)
+    df = pd.DataFrame(
+        {
+            "store": [1, 1, 1, 1, 2, 2, 2, 2],
+            "date": [
+                date(2022, 1, 1),
+                date(2022, 1, 2),
+                date(2022, 1, 3),
+                date(2022, 1, 4),
+                date(2022, 1, 10),
+                date(2022, 1, 11),
+                date(2022, 1, 12),
+                date(2022, 1, 13),
+            ],
+            "sales": [100, 150, 200, 250, 110, 165, 220, 260],
+        }
+    )
+    df2 = series_to_supervised(
+        df, groupby_cols=["store"], value_cols=["sales"], window=1, lag=1, dropnan=True
+    )
     assert len(df2) == 4
     assert all(df2.iloc[0].values == [1, date(2022, 1, 2), 150, 100.0, 200.0])
     assert all(df2.iloc[1].values == [1, date(2022, 1, 3), 200, 150.0, 250.0])
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -3,8 +3,15 @@
 import pandas as pd
 from typing import List
 
-def series_to_supervised(data: pd.DataFrame, groupby_cols: List[str], value_cols: List[str],
-                         window=1, lag=1, dropnan=True) -> pd.DataFrame():
+
+def series_to_supervised(
+    data: pd.DataFrame,
+    groupby_cols: List[str],
+    value_cols: List[str],
+    window=1,
+    lag=1,
+    dropnan=True,
+) -> pd.DataFrame():
     """Use history according to `window` size to predict the future specified in `lag`.
     The output is a wide-format dataframe. This method creates (1, ..., window) features for each of the columns in `data`.
 
@@ -15,24 +22,28 @@ def series_to_supervised(data: pd.DataFrame, groupby_cols: List[str], value_cols
         window (int, optional): History window we would want to use for prediction (applies to `value_cols`). Defaults to 1.
         lag (int, optional): Future period we want to predict (applies to `value_cols`). Defaults to 1.
         dropnan (bool, optional): When we shift `val_cols` some values would be Null, by default the functions removes these records. Defaults to True.
-    
+
     Returns:
         pd.DataFrame(): a wide-format dataframe which includes all `val_cols` that are shifted according to `window`.
     """
     cols = list()
-    
+
     # Current time step (t=0)
     new_names = {name: f"{name}(t)" for name in value_cols}
     cols.append(data.rename(columns=new_names))
-    
+
     # Input sequence (t-n, ... t-1)
     for i in range(window, 0, -1):
         new_names = {name: f"{name}(t-{i})" for name in value_cols}
-        cols.append(data.groupby(groupby_cols)[value_cols].shift(i).rename(columns=new_names))
-        
+        cols.append(
+            data.groupby(groupby_cols)[value_cols].shift(i).rename(columns=new_names)
+        )
+
     # Target time step (t=lag)
     new_names = {name: f"{name}(t+{lag})" for name in value_cols}
-    cols.append(data.groupby(groupby_cols)[value_cols].shift(-lag).rename(columns=new_names))
+    cols.append(
+        data.groupby(groupby_cols)[value_cols].shift(-lag).rename(columns=new_names)
+    )
     # Put it all together
     agg = pd.concat(cols, axis=1)
     # Drop rows with NaN values
